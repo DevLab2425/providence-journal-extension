a644c4d67ebaa1247bd6ebc62c637a4af09ba65a
Barry, Paul <devlab2425@gmail.com>
2015-01-30 09:57:17 -0500

    Minor updates

--- scripts/actionScripts.js
+++ scripts/actionScripts.js
@@ -1,5 +1,4 @@
 (function(chrome){
-	// Called when the user clicks on the browser action icon.
 	chrome.browserAction.onClicked.addListener(function() {
 		chrome.tabs.executeScript(null, {file: "scripts/hideDigitalSubscriptionModal.js"});
 	});
